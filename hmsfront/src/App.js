44c633051616ddfce25ff4a66c20177d078fde2e
SaurabhPotdukhe <118752436+potdukhe12@users.noreply.github.com>
2023-11-02 22:34:45 +0530

    Update App.js

--- hmsfront/src/App.js
+++ hmsfront/src/App.js
@@ -28,7 +28,7 @@ import { Aboutus } from './components/common/Aboutus';
 
 function App() {
 
-  const path = "http://13.233.152.221:8080";
+  const path = "http://3.110.177.253:8080";
   // const path = "http://localhost:8080";
 
   return (
